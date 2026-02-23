4a2bc4fabf55c1aa79de242949d11147bbac4d64
SeleApp <seleapp.info@gmail.com>
2026-02-23 10:16:18 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+$text1 = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perche'";
+$text6 = "$text2";
+$text7 = "bevuto";
+$text8 = "tutto";
+
+$textConcatenato = $text1 . " " . ucfirst($text2) . " " . $text3 . $text4 . " " . $text5 . " " . $text6 . " " . $text7 . " " . $text8 . ".";
+echo $textConcatenato;
